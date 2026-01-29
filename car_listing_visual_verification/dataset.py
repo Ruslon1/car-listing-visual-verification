60f167a5198326a43bff5fcb980ba39be69d7b3d
Ruslan <84863005+Ruslon1@users.noreply.github.com>
2026-01-29 21:11:18 +0500

    training

--- car_listing_visual_verification/dataset.py
+++ car_listing_visual_verification/dataset.py
@@ -1,23 +1,24 @@
 from pathlib import Path
-
-from loguru import logger
-from tqdm import tqdm
-import typer
-
-from car_listing_visual_verification.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 import scipy.io
 import pandas as pd
+import typer
+from loguru import logger
+from sklearn.model_selection import train_test_split  # <--- Make sure to pip install scikit-learn
 
+from car_listing_visual_verification.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
 app = typer.Typer()
 
+
 @app.command()
 def main(
-    train_mat: Path = RAW_DATA_DIR / "car_devkit/devkit/cars_train_annos.mat",
-    meta_mat: Path = RAW_DATA_DIR / "car_devkit/devkit/cars_meta.mat",
-    output_path: Path = PROCESSED_DATA_DIR / "cars_train.csv",
+        train_mat: Path = RAW_DATA_DIR / "car_devkit/devkit/cars_train_annos.mat",
+        meta_mat: Path = RAW_DATA_DIR / "car_devkit/devkit/cars_meta.mat",
+        train_output: Path = PROCESSED_DATA_DIR / "train.csv",
+        val_output: Path = PROCESSED_DATA_DIR / "val.csv",
+        split_ratio: float = 0.1,
+        seed: int = 42
 ):
-    logger.info("Loading")
     train_annotations = scipy.io.loadmat(train_mat)["annotations"]
     meta = scipy.io.loadmat(meta_mat)["class_names"]
 
@@ -25,6 +26,7 @@ def main(
     for annotation in train_annotations[0]:
         fname = str(annotation["fname"][0])
         class_id = int(annotation["class"][0][0])
+
         x1 = int(annotation["bbox_x1"][0][0])
         y1 = int(annotation["bbox_y1"][0][0])
         x2 = int(annotation["bbox_x2"][0][0])
@@ -32,6 +34,7 @@ def main(
 
         class_index = class_id - 1
         class_name = meta[0][class_index][0]
+
         rows.append({
             "image_path": fname,
             "class_id": class_id,
@@ -43,9 +46,20 @@ def main(
         })
 
     df = pd.DataFrame(rows)
-    output_path.parent.mkdir(parents=True, exist_ok=True)
-    df.to_csv(output_path, index=False)
-    logger.info("Exported to csv")
+
+    logger.info(f"Splitting data")
+
+    train_df, val_df = train_test_split(
+        df,
+        test_size=split_ratio,
+        random_state=seed,
+        stratify=df["class_id"]
+    )
+
+    train_output.parent.mkdir(parents=True, exist_ok=True)
+
+    train_df.to_csv(train_output, index=False)
+    val_df.to_csv(val_output, index=False)
 
 if __name__ == "__main__":
     app()
--- car_listing_visual_verification/modeling/train.py
+++ car_listing_visual_verification/modeling/train.py
@@ -1,29 +1,126 @@
 from pathlib import Path
 
+import torch
 from loguru import logger
 from tqdm import tqdm
 import typer
+import torch.nn as nn
+import torch.optim as optim
 
-from car_listing_visual_verification.config import MODELS_DIR, PROCESSED_DATA_DIR
+from car_listing_visual_verification.config import MODELS_DIR, PROCESSED_DATA_DIR, RAW_DATA_DIR
+import car_listing_visual_verification.data_loader as data_loader
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from torchvision.transforms import transforms
+from torch.utils.data import DataLoader
+from torchvision import models
+
+
+imagenet_mean = [0.485, 0.456, 0.406]
+imagenet_std = [0.229, 0.224, 0.225]
+
+train_transforms = transforms.Compose([
+    transforms.Resize((224, 224)),
+
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomRotation(degrees=10),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+
+    transforms.ToTensor(),
+    transforms.Normalize(imagenet_mean, imagenet_std)
+])
+
+val_transforms = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(imagenet_mean, imagenet_std)
+])
 
 app = typer.Typer()
 
 
 @app.command()
 def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    # -----------------------------------------
+        train_path: Path = PROCESSED_DATA_DIR / "train.csv",
+        images_dir: Path = RAW_DATA_DIR / "cars_train/cars_train",
+        validation_path: Path = PROCESSED_DATA_DIR / "val.csv",
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Training some model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Modeling training complete.")
-    # -----------------------------------------
+    train_dataset = data_loader.StanfordCarsDataset(train_path, images_dir, train_transforms)
+    val_dataset = data_loader.StanfordCarsDataset(validation_path, images_dir, val_transforms)
+
+    logger.info("init dataloaders")
+    train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=4)
+    val_dataloader = DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=4)
+
+    device = torch.device('mps')
+
+    logger.info("Device: " + device.__str__())
+
+    model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, 196)
+
+    model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+
+    adam = optim.Adam(model.fc.parameters(), lr=0.001)
+
+    num_epochs = 10
+
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+        correct_predictions = 0
+        total_samples = 0
+
+        progress = tqdm(train_dataloader, desc=f"Epoch {epoch+1}/{num_epochs}")
+
+        for inputs, labels in progress:
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            adam.zero_grad()
+
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            loss.backward()
+            adam.step()
+
+            running_loss += loss.item() * inputs.size(0)
+            _, predicted = torch.max(outputs, 1)
+            correct_predictions += (predicted == labels).sum().item()
+            total_samples += labels.size(0)
+
+            progress.set_postfix(loss=loss.item())
+
+        epoch_loss = running_loss / total_samples
+        epoch_acc = correct_predictions / total_samples
+
+        logger.info(f"Epoch {epoch + 1} Results - Loss: {epoch_loss:.4f} - Acc: {epoch_acc:.4f}")
+
+        model.eval()
+        val_correct = 0
+        val_total = 0
+
+        with torch.no_grad():
+            for inputs, labels in val_dataloader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs, 1)
+                val_total += labels.size(0)
+                val_correct += (predicted == labels).sum().item()
+
+        val_acc = val_correct / val_total
+        logger.success(f"Validation Accuracy: {val_acc:.4f}")
+
+        torch.save(model.state_dict(), MODELS_DIR / "model.pkl")
+
+    logger.success("Training Complete")
 
 
 if __name__ == "__main__":
