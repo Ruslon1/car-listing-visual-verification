d36f0f36ffce47b6884aa4fd4f1065bcc376cd95
Ruslan <84863005+Ruslon1@users.noreply.github.com>
2026-02-26 19:40:06 +0500

    refactor and predicting

--- car_listing_visual_verification/inference.py
+++ car_listing_visual_verification/inference.py
@@ -1,57 +1,26 @@
 from argparse import ArgumentParser
 from pathlib import Path
 
-import pandas as pd
-import torch
 from loguru import logger
-from PIL import Image
-from torch import nn
-from torchvision import models
 
 from car_listing_visual_verification.config import (
     HF_RELEASE_CLASS_MAPPING_FILE,
     MODELS_DIR,
 )
-from car_listing_visual_verification.train import device, val_transforms
-
+from car_listing_visual_verification.modeling.predictor import Predictor
 
 def infer(
-    image_path: Path,
-    checkpoint_path: Path = MODELS_DIR / "model.pkl",
-    class_mapping_path: Path = HF_RELEASE_CLASS_MAPPING_FILE,
+        image_path: Path,
+        checkpoint_path: Path = MODELS_DIR / "model.pkl",
+        class_mapping_path: Path = HF_RELEASE_CLASS_MAPPING_FILE,
 ):
     if not image_path.exists():
         raise FileNotFoundError(f"Image not found: {image_path}")
     if not checkpoint_path.exists():
         raise FileNotFoundError(f"Checkpoint not found: {checkpoint_path}")
 
-    class_mapping_df = pd.read_parquet(class_mapping_path).sort_values("class_id")
-    class_mapping = dict(
-        zip(
-            class_mapping_df["class_id"].astype(int).tolist(),
-            class_mapping_df["class_name"].astype(str).tolist(),
-        )
-    )
-
-    model = models.resnet50(weights=None)
-    model.fc = nn.Linear(model.fc.in_features, 99)
-    model.load_state_dict(torch.load(checkpoint_path, map_location=device))
-    model.to(device)
-    model.eval()
-
-    with Image.open(image_path) as img:
-        image = img.convert("RGB")
-
-    image_tensor = val_transforms(image).unsqueeze(0).to(device)
-
-    with torch.no_grad():
-        logits = model(image_tensor)
-        probs = torch.softmax(logits, dim=1)
-        confidence, pred_class = torch.max(probs, dim=1)
-
-    class_id = int(pred_class.item())
-    class_name = class_mapping.get(class_id, str(class_id))
-    score = float(confidence.item())
+    predictor = Predictor(checkpoint_path, class_mapping_path)
+    class_id, class_name, score = predictor.predict(image_path)
 
     logger.success(f"Prediction: {class_name} (class_id={class_id}, prob={score:.4f})")
 
---
+++ car_listing_visual_verification/modeling/predictor.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+
+import pandas as pd
+import torch
+from PIL import Image
+from loguru import logger
+from torch import nn
+from torchvision import models, transforms
+
+from car_listing_visual_verification.config import (
+    HF_RELEASE_CLASS_MAPPING_FILE,
+    MODELS_DIR,
+)
+
+imagenet_mean = [0.485, 0.456, 0.406]
+imagenet_std = [0.229, 0.224, 0.225]
+
+inference_transforms = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(imagenet_mean, imagenet_std)
+])
+
+
+class Predictor:
+    def __init__(self, checkpoint_path: Path = MODELS_DIR / "model.pkl",
+                 class_mapping_path: Path = HF_RELEASE_CLASS_MAPPING_FILE,
+                 image_transforms: transforms.Compose = inference_transforms, device=None):
+        self.model = None
+        self.checkpoint_path = checkpoint_path
+        self.class_mapping_path = class_mapping_path
+        self.image_transforms = image_transforms
+
+        self.device = device or torch.device(
+            "cuda" if torch.cuda.is_available()
+            else "mps" if torch.backends.mps.is_available()
+            else "cpu"
+        )
+
+        class_mapping_df = pd.read_parquet(class_mapping_path).sort_values("class_id")
+        self.class_mapping = dict(
+            zip(
+                class_mapping_df["class_id"].astype(int).tolist(),
+                class_mapping_df["class_name"].astype(str).tolist(),
+            )
+        )
+
+        self.load_model()
+
+    def load_model(self):
+        if not self.checkpoint_path.exists():
+            raise FileNotFoundError(f"Checkpoint not found: {self.checkpoint_path}")
+
+        self.model = models.resnet50(weights=None)
+        self.model.fc = nn.Linear(self.model.fc.in_features, 99)
+        self.model.load_state_dict(torch.load(self.checkpoint_path, map_location=self.device))
+        self.model.to(self.device)
+        self.model.eval()
+
+    def predict(self, image_path: Path,):
+        if not image_path.exists():
+            raise FileNotFoundError(f"Image not found: {image_path}")
+
+        with Image.open(image_path) as img:
+            image = img.convert("RGB")
+
+        image_tensor = self.image_transforms(image).unsqueeze(0).to(self.device)
+
+        with torch.no_grad():
+            logits = self.model(image_tensor)
+            probs = torch.softmax(logits, dim=1)
+            confidence, pred_class = torch.max(probs, dim=1)
+
+        class_id = int(pred_class.item())
+        class_name = self.class_mapping.get(class_id, str(class_id))
+        score = float(confidence.item())
+
+        logger.success(f"Prediction: {class_name} (class_id={class_id}, prob={score:.4f})")
+        return class_id, class_name, score
